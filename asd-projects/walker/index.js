0cc0fdc1bf9d2bd509709d05e925007984cc2554
YinY23 <yinyou@yahoo.com>
2025-10-09 23:50:49 +0000

    finished all of walker minus 1 extra

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,9 +10,30 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  const KEY = {
+    ENTER: 13,
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+    w: 87,
+    a: 65,
+    s: 83,
+    d: 68,
+    W: 119,
+    A: 97, 
+    S: 115,
+    D: 100,
+  };
   
   // Game Item Objects
 
+  var walker = {
+    x: 0,
+    y: 0, 
+    speedX: 0,
+    speedY: 0
+  };
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -23,7 +44,9 @@ function runProgram(){
 
   Note: You can have multiple event listeners for different types of events.
   */
-  $(document).on('eventType', handleEvent);                          
+  $(document).on('keydown', handleKeyDown);
+  $(document).on("keyup", handleKeyUp);
+  $(document).on("click", handleClick);
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -35,6 +58,9 @@ function runProgram(){
   */
   function newFrame() {
     
+    repositionGameItem();
+    wallCollision();
+    redrawGameItem()
 
   }
   
@@ -44,7 +70,49 @@ function runProgram(){
   
   Note: You can have multiple event handlers for different types of events.
   */
-  function handleEvent(event) {
+  function handleKeyDown(event) {
+
+    if (event.which === KEY.LEFT || event.which === KEY.a || event.which === KEY.A) {
+      walker.speedX -= 5;
+    }
+    else if (event.which === KEY.DOWN || event.which === KEY.s || event.which === KEY.S) {
+      walker.speedY += 5;
+    }
+    else if (event.which === KEY.RIGHT || event.which === KEY.d || event.which === KEY.D) {
+      walker.speedX += 5;
+    }
+    else if (event.which === KEY.UP || event.which === KEY.w || event.which === KEY.W) {
+      walker.speedY -= 5;
+    }
+
+  }
+
+  function handleKeyUp(event) {
+
+    if (event.which === KEY.LEFT || event.which === KEY.a || event.which === KEY.A) {
+      walker.speedX = 0;
+    }
+    else if (event.which === KEY.DOWN || event.which === KEY.s || event.which === KEY.S) {
+      walker.speedY = 0;
+    }
+    else if (event.which === KEY.RIGHT || event.which === KEY.d || event.which === KEY.D) {
+      walker.speedX = 0;
+    }
+    else if (event.which === KEY.UP || event.which === KEY.w || event.which === KEY.W) {
+      walker.speedY = 0;
+    }
+
+  }
+
+  function handleClick() {
+
+    let rng1 = Math.floor(Math.random() * 255);
+    let rng2 = Math.floor(Math.random() * 255);
+    let rng3 = Math.floor(Math.random() * 255);
+
+    console.log(rng1, rng2, rng3);
+
+    $("#walker").css("background-color", "rgb(" + rng1 + ", " + rng2 + ", " + rng3 + ")")
 
   }
 
@@ -52,7 +120,40 @@ function runProgram(){
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  
+  function repositionGameItem() {
+    walker.x += walker.speedX;
+    walker.y += walker.speedY;
+  }
+
+  function redrawGameItem() {
+    let unit = $("#walker");
+    unit.css("left", walker.x);
+    unit.css("top", walker.y);
+  }
+
+  function wallCollision() {
+
+    let board = $("#board");
+    let unit = $("#walker");
+
+    if (walker.x < 0) {
+      walker.speedX = 0;
+      walker.x = 0;
+    }
+    if (walker.y < 0) {
+      walker.speedY = 0;
+      walker.y = 0;
+    }
+    if (walker.x > board.width() - unit.width()) {
+      walker.speedX = 0;
+      walker.x = board.width() - unit.width();
+    }
+    if (walker.y > board.height() - unit.height()) {
+      walker.speedY = 0;
+      walker.y = board.height() - unit.height();
+    }
+  }
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
