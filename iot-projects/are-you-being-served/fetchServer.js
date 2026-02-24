512ce6e23bd293ab929e1ba821e8831ca2d80a30
Yin You <yin.you@yahoo.com>
2026-02-24 00:54:04 -0600

    finished are-you-being-served

--- iot-projects/are-you-being-served/fetchServer.js
+++ iot-projects/are-you-being-served/fetchServer.js
@@ -1,1 +1,24 @@
 // fetchServer.js file
+var args = process.argv.slice(2);
+var url = args[0] ? args[0] : "https://yiny23.github.io/portfolio.html";
+const http = require("http");
+const port = 8686;
+
+http
+  .createServer(async function (req, res) {
+    if (args[1].toLowerCase() == "html") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+    }
+    if (args[1].toLowerCase() == "text") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+    }
+    let fetchResponse = await fetch(url);
+    if (fetchResponse.ok) {
+      let html = await fetchResponse.text();
+      res.write(html);
+    } else {
+      res.write(fetchResponse.statusText + " " + fetchResponse.status);
+    }
+    res.end();
+  })
+  .listen(port);
