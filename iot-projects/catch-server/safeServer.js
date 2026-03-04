0eb9272155e348c9b8fc895084d796465d8fa591
YinY23 <yinyou@yahoo.com>
2026-03-03 19:05:41 -0600

    finished catch-server

--- iot-projects/catch-server/safeServer.js
+++ iot-projects/catch-server/safeServer.js
@@ -6,24 +6,45 @@ const server = http.createServer(requestHandling).listen(port);
 
 function requestHandling(req, res) {
   try {
-    switch (req.getResponse()) {
+    switch (req.method) {
       case "GET":
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/plain");
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.write(serverStatus.status); //method 1 continued
         break;
       case "PUT":
         let body = "";
         req.on("data", function (chunk) {
-          body += chunk.toString();
+          body += chunk;
         });
         req.on("end", function () {
-          res.statusCode = 200; // change this to only one line
+          serverStatus = JSON.parse(body);
+          // serverStatus.status = JSON.parse(body);
         });
+        res.writeHead(200, { "Content-Type": "text/plain" }); //method 2
+        res.write("The server has been updated.");
+        break;
+      case "DELETE":
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        let tempStatus = serverStatus.status;
+        serverStatus = undefined;
+        res.write("Successfully deleted data:" + tempStatus);
+        break;
+      case "POST":
+        let body2 = "";
+        req.on("data", function (chunk) {
+          body2 += chunk;
+        });
+        req.on("end", function () {
+          serverStatus.messages = [];
+          serverStatus.messages.push(body2);
+        });
+
         break;
     }
   } catch (e) {
+    console.log(e);
     res.statusCode = 500;
-    res.write("The server has no data.\n");
+    res.write("The server has no data.");
   } finally {
     res.write("-and the message arrived");
     res.end();
